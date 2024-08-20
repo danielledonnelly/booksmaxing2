3637512c6a2ce72f6168a6e66be9d54cbf68face
Danielle Donnelly <145680391+danielledonnelly@users.noreply.github.com>
2024-08-20 21:13:23 -0230

    tweaks

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Grid, Card, CardContent, Typography } from '@mui/material';
-import StreaksCalendar from './components/StreaksCalendar.jsx';
+import StreaksCalendar from './components/StreaksCalendar';
 import BookTracker from './components/BookTracker';
 import PromptGenerator from './components/PromptGenerator';
 import Library from './components/Library';
@@ -15,7 +16,7 @@ const Home = () => {
         <Grid item xs={12} sm={3} style={{ padding: '0 8px', display: 'flex', justifyContent: 'center' }}>
           <Card className="card">
             <CardContent>
-              <Typography variant="h5">Streaks + Calendar</Typography>
+              <Typography variant="h5">Calendar</Typography>
               <StreaksCalendar entryCounts={entryCounts} setEntryCounts={setEntryCounts} />
             </CardContent>
           </Card>
@@ -24,7 +25,7 @@ const Home = () => {
         <Grid item xs={12} sm={3} style={{ padding: '0 8px', display: 'flex', justifyContent: 'center' }}>
           <Card className="card">
             <CardContent>
-              <Typography variant="h5">Track Your Reading</Typography>
+              <Typography variant="h5">Reading Tracker</Typography>
               <BookTracker entryCounts={entryCounts} setEntryCounts={setEntryCounts} />
             </CardContent>
           </Card>
